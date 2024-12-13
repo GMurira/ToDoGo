2e5fdd7df377c83e1086596d7390553e80706b84
GMurira <nguyaimurira@gmail.com>
2024-12-13 21:53:16 +0300

    Initial Commit:

--- Todo.go
+++ Todo.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Lets GO")
+	shortGoalng := "Watch Go crash"
+	fullGolang := "Watch Hull Course"
+	reward := "Reward myself with a Drink"
+
+	taskItems := []string{shortGoalng, fullGolang, reward}
+
+	fmt.Println()
+	fmt.Println("Here are the tasks TODO")
+
+	for index, task := range taskItems {
+		fmt.Println(index, task)
+	}
+
 }
