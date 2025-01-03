65f96e10179654f6ee0aceb3714700ecaa762a86
GMurira <nguyaimurira@gmail.com>
2025-01-03 07:30:01 +0300

    seperation of logic

--- Todo.go
+++ Todo.go
@@ -3,17 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	shortGoalng := "Watch Go crash"
-	fullGolang := "Watch Hull Course"
-	reward := "Reward myself with a Drink"
-
-	taskItems := []string{shortGoalng, fullGolang, reward}
-
 	fmt.Println()
+	var fullGolang = "Watch Hull Course"
+	var reward = "Reward myself with a Drink"
+	var taskItems = []string{fullGolang, reward}
+
+	printTasks(taskItems)
+	fmt.Println()
+	taskItems = addTasks(taskItems, "Go for a run")
+	fmt.Println("Updated List")
+	printTasks(taskItems)
+}
+
+func printTasks(taskItems []string) {
 	fmt.Println("Here are the tasks TODO")
 
 	for index, task := range taskItems {
-		fmt.Println(index, task)
+		//fmt.Println(index+1, ":", task)
+		fmt.Printf("%d: %s\n", index+1, task)
 	}
+}
 
+func addTasks(taskItems []string, newTask string) []string {
+	var updatedTasks = append(taskItems, newTask)
+	return updatedTasks
 }
